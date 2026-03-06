1cb246083208284e8e35605681a5f24bae6b0339
jvm <mocitodev@pm.me>
2026-03-06 19:24:51 +0000

    Fix mobie formula checksum for public repository

--- Formula/mobie.rb
+++ Formula/mobie.rb
@@ -2,7 +2,7 @@ class Mobie < Formula
   desc "Standalone CLI for querying the MOBIE API"
   homepage "https://github.com/jvm/mobie-cli"
   url "https://github.com/jvm/mobie-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "43e1d8227ee479b9b068b0d05e175478b577380e3025878dcaa3664c84d619df"
   license "ISC"
   head "https://github.com/jvm/mobie-cli.git", branch: "main"
 
